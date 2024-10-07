28c34f19459c9fd9a249f9c77ce47f3e72ae0d32
Robert <54269637+R-udren@users.noreply.github.com>
2024-10-07 21:52:47 +0300

    Added router

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+import Contact from '../pages/Contact.vue';
+import Portfolio from '../pages/Portfolio.vue';
+
+export default [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  { path: '/contact', component: Contact },
+  { path: '/portfolio', component: Portfolio },
+];
