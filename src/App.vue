0d045809d51a21825cec22f1375afe9249628ad8
Robert <54269637+R-udren@users.noreply.github.com>
2024-10-07 21:54:34 +0300

    My template for app

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,22 @@
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
 <template>
+  <div id="app">
+    <Header/>
+    <router-view/>
+  </div>
 </template>
+
+<style>
+#app {
+  font-family: 'Inter', sans-serif;
+}
+</style>
