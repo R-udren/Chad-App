e517755e8ad9293d5c8922c5a6774ccd782915c5
Robert <54269637+R-udren@users.noreply.github.com>
2024-10-10 22:03:52 +0300

    Added links and made footer bg darker

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,18 +6,38 @@
   <footer class="app-footer">
     <div class="footer-content">
       <p>&copy; 2024 ChadApp. All rights reserved.</p>
+      <div class="links">
+        <a href="https://github.com/R-udren" target="_blank" rel="noopener noreferrer">
+          <img src="../assets/github.svg" alt="Github"/>
+        </a>
+      </div>
     </div>
   </footer>
 </template>
 
 <style scoped>
-footer {
+.footer-content {
   text-align: center;
   padding-top: var(--spacing);
-  background-color: var(--background);
+  background-color: var(--second-background);
   color: var(--subtext);
   position: absolute;
   width: 100%;
   bottom: 0;
+  transition: background-color 0.3s ease, color 0.3s ease;
+}
+
+.footer-content p {
+  margin: 0;
+}
+
+.footer-content .links img {
+  width: 24px;
+  height: 24px;
+  filter: invert(0.2);
+}
+
+.footer-content .links a {
+  margin: 0 0.5rem;
 }
 </style>
