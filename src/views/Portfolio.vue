eda8f3950f77051959f19f1a957fbed20d930f6d
Robert <54269637+R-udren@users.noreply.github.com>
2024-10-11 22:48:08 +0300

    Small text changes

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -13,15 +13,15 @@ import Main from "../components/Main.vue";
         <img src="https://i.pinimg.com/originals/8e/de/53/8ede538fcf75a0a1bd812810edb50cb7.jpg" alt="Picture">
       </div>
       <h1>My Portfolio</h1>
-      <p>Take a look at my recent projects.</p>
+      <p>Here are some of my recent projects:</p>
       <div class="cards-container">
-        <Card title="SteamAccountsFinder" text="Find and display all Steam Accounts found on PC"
+        <Card title="SteamAccountsFinder" text="Find and display all Steam Accounts found on PC."
               buttonText="(Private Project)" link="/" :image="steamAccountsImage"/>
-        <Card title="ToDoList" text="Console task management app. Basic CRUD with registration and database"
+        <Card title="ToDoList" text="Console task management app with basic CRUD operations, registration, and database."
               buttonText="View Project" link="https://github.com/R-udren/ToDoList" :image="toDoListImage"/>
         <Card title="AI Assistant" :image="aiAssistantImage"
-              text="This project is an AI Assistant built using Python.
-            It uses OpenAI's models for conversation and the Whisper API for speech recognition."
+              text="This project is an AI assistant built using Python.
+              It uses OpenAI's models for conversation and the Whisper API for speech recognition."
               buttonText="View Project" link="https://github.com/R-udren/VoiceChatOver9000"/>
       </div>
     </div>
