5ac1e6b6e701efee44107a93ff4668e0c69631de
Robert <54269637+R-udren@users.noreply.github.com>
2024-10-11 22:19:44 +0300

    Uploaded some projects to portfolio

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,22 +1,31 @@
 <script setup>
 import Card from "../components/Card.vue";
 import steamAccountsImage from "../assets/SteamAccountsConsole.png";
+import toDoListImage from "../assets/ToDoList.png";
+import aiAssistantImage from "../assets/AIChat.png";
+import Main from "../components/Main.vue";
 </script>
 
 <template>
-  <div class="portfolio">
-    <div class="avatar">
-      <img src="https://i.pinimg.com/originals/8e/de/53/8ede538fcf75a0a1bd812810edb50cb7.jpg" alt="Picture">
+  <Main>
+    <div class="portfolio">
+      <div class="avatar">
+        <img src="https://i.pinimg.com/originals/8e/de/53/8ede538fcf75a0a1bd812810edb50cb7.jpg" alt="Picture">
+      </div>
+      <h1>My Portfolio</h1>
+      <p>Take a look at my recent projects.</p>
+      <div class="cards-container">
+        <Card title="SteamAccountsFinder" text="Find and display all Steam Accounts found on PC"
+              buttonText="(Private Project)" link="/" :image="steamAccountsImage"/>
+        <Card title="ToDoList" text="Console task management app. Basic CRUD with registration and database"
+              buttonText="View Project" link="https://github.com/R-udren/ToDoList" :image="toDoListImage"/>
+        <Card title="AI Assistant" :image="aiAssistantImage"
+              text="This project is an AI Assistant built using Python.
+            It uses OpenAI's models for conversation and the Whisper API for speech recognition."
+              buttonText="View Project" link="https://github.com/R-udren/VoiceChatOver9000"/>
+      </div>
     </div>
-    <h1>My Portfolio</h1>
-    <p>Take a look at my recent projects.</p>
-    <div class="cards-container">
-      <Card title="SteamAccounts" text="Find and display all Steam Accounts found on PC" buttonText="View Project"
-            link="/" :image="steamAccountsImage"/>
-      <Card title="Project 2" text="This is a project description." buttonText="View Project" link="/"/>
-      <Card title="Project 3" text="This is a project description." buttonText="View Project" link="/"/>
-    </div>
-  </div>
+  </Main>
 </template>
 
 <style scoped>
